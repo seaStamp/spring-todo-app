08df244f80932a3969cd534fb905ade0e945ff82
seastamp <seastamp10@gmail.com>
2023-12-04 03:28:08 +0900

    feat: @EnableJpaAuditing Application에서 제거

--- src/main/java/com/sparta/todoapp/TodoAppApplication.java
+++ src/main/java/com/sparta/todoapp/TodoAppApplication.java
@@ -4,9 +4,7 @@ import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class TodoAppApplication {
 
