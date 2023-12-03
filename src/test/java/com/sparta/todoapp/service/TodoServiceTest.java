55c0c40d0739f026df6eb100677f6c0529788e41
seastamp <seastamp10@gmail.com>
2023-12-04 07:35:36 +0900

    test(todo): TodoService todo 단건조회 테스트

--- src/test/java/com/sparta/todoapp/service/TodoServiceTest.java
+++ src/test/java/com/sparta/todoapp/service/TodoServiceTest.java
@@ -11,6 +11,7 @@ import com.sparta.todoapp.dto.TodoRequestDto;
 import com.sparta.todoapp.dto.TodoResponseDto;
 import com.sparta.todoapp.entity.Todo;
 import com.sparta.todoapp.entity.User;
+import com.sparta.todoapp.exception.NotFoundException;
 import com.sparta.todoapp.repository.TodoRepository;
 import com.sparta.todoapp.repository.UserRepository;
 import java.util.Optional;
@@ -57,5 +58,34 @@ class TodoServiceTest {
         verify(todoRepository, times(1)).save(any(Todo.class));
     }
 
+    @Test
+    @DisplayName("todo 단건 조회 테스트 : 성공")
+    void getTodoSuccessTest() {
+        // given
+        Long existingTodoId = 1L;
+        Todo existingTodo = new Todo(new TodoRequestDto("테스트 제목", "테스트 유저"), testUser);
+        given(todoRepository.findById(existingTodoId)).willReturn(Optional.of(existingTodo));
 
+        // when
+        TodoResponseDto responseDto = todoService.getTodo(existingTodoId);
+
+        // then
+        assertNotNull(responseDto);
+        assertEquals(existingTodo.getTitle(), responseDto.getTitle());
+        assertEquals(existingTodo.getContent(), responseDto.getContent());
+    }
+
+    @Test
+    @DisplayName("todo 단건 조회 테스트 : 실패")
+    void getTodoFailTest() {
+        // given
+        Long nonExistingTodoId = 1L;
+        given(todoRepository.findById(nonExistingTodoId)).willReturn(Optional.empty());
+
+        // when
+        Exception exception = assertThrows(NotFoundException.class, () -> todoService.getTodo(nonExistingTodoId));
+
+        // then
+        assertEquals("존재하지 않는 할일 ID 입니다", exception.getMessage());
+    }
 }
