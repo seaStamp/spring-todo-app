af3b1366aaa6d1df66abb2da19154dffe6c5839f
seastamp <seastamp10@gmail.com>
2023-12-14 20:14:59 +0900

    test(user) SignupRequestDto 테스트

---
+++ src/test/java/com/sparta/todoapp/dto/SignupRequestDtoTest.java
@@ -0,0 +1,55 @@
+package com.sparta.todoapp.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class SignupRequestDtoTest {
+
+    @Nested
+    @DisplayName("유저 요청 DTO 생성")
+    class createSignupRequestDto {
+        @Test
+        @DisplayName("유저 요청 DTO생성 성공")
+        void createSignupRequestDto_success() {
+            // given
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            signupRequestDto.setUsername("user123");
+            signupRequestDto.setPassword("12345678");
+
+            // when
+            Set<ConstraintViolation<SignupRequestDto>> violations = validate(signupRequestDto);
+
+            // then
+            assertThat(violations).isEmpty();
+        }
+
+        @Test
+        @DisplayName("유저 요청 DTO생성 실패")
+        void createSignupRequestDto_fail_wrongUserName() {
+            // Given
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            signupRequestDto.setUsername("Invalid user name");
+            signupRequestDto.setPassword("12345678");
+
+            // When
+            Set<ConstraintViolation<SignupRequestDto>> violations = validate(signupRequestDto);
+
+            // Then
+        }
+    }
+
+    private Set<ConstraintViolation<SignupRequestDto>> validate(SignupRequestDto signupRequestDto) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        return validator.validate(signupRequestDto);
+    }
+}
