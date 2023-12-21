7d4955148fc1818f141b56c71303605a201cc1ea
seastamp <seastamp10@gmail.com>
2023-12-22 01:00:55 +0900

    fix(aop) : Aop 반환값 수정

--- src/main/java/com/sparta/todoapp/global/aop/TimeAop.java
+++ src/main/java/com/sparta/todoapp/global/aop/TimeAop.java
@@ -11,21 +11,24 @@ import org.springframework.stereotype.Component;
 @Slf4j(topic = "걸린 시간 로그")
 public class TimeAop {
 
-    long startTime;
-
     @Around("execution(* com.sparta.todoapp.domain.*.controller..*(..))")
-    public void timeLog(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object timeLog(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
 
+        Object result;
+
         try {
-            joinPoint.proceed();
+            result = joinPoint.proceed();
             log.info("실행된 Method: {} | 처리 결과 : OK | 걸린 시간 : {}ms",
                 joinPoint.getSignature().getName(),
                 System.currentTimeMillis() - startTime);
         } catch (Exception ex) {
             log.error("실행된 Method: {} | 처리 결과 : Failed | 걸린 시간 : {}ms",
-                joinPoint.getSignature().getName(), System.currentTimeMillis() - startTime);
+                joinPoint.getSignature().getName(),
+                System.currentTimeMillis() - startTime);
             throw ex;
         }
+
+        return result;
     }
 }
